053828e1fca28a5b86633de0d93dc9dbc619d815
Cin💜 Lo <cinlos3382@gmail.com>
2023-05-06 12:52:03 -0300

    tipados; tipos inferidos

---
+++ src/03-typing.ts
@@ -0,0 +1,9 @@
+let myProductName = 'Product 1';
+let myProductPrice = 123;
+
+const myProductStock = 1000;
+
+myProductName = 'change'
+myProductName.toUpperCase()
+
+myProductPrice.toString()
---
+++ src/o3-demo.ts
@@ -0,0 +1,13 @@
+(()=> {
+   let myProductName = 'Product 1';
+    let myProductPrice = 123;
+
+    const myProductStock = 1000;
+
+    myProductName = 'change'
+    myProductName.toUpperCase()
+
+    myProductPrice.toString() 
+})();
+
+
